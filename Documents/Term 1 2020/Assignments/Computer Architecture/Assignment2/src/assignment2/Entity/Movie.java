4c1ec88ee56f8fb2344344af56e2632d71387d0f
gauravprsnt <prsnt.gaurav@gmail.com>
2020-05-06 12:12:21 +1000

    Removing the imports

--- Documents/Term 1 2020/Assignments/Computer Architecture/Assignment2/src/assignment2/Entity/Movie.java
+++ Documents/Term 1 2020/Assignments/Computer Architecture/Assignment2/src/assignment2/Entity/Movie.java
@@ -7,9 +7,6 @@ package assignment2.Entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
